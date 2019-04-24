bf91d9f9d53a9f7cde9ea133fa5c0b58edbb5d75
F. Stephen Kirschbaum <fskirschbaum@gmail.com>
2019-04-23 22:49:33 -0500

    Completes Tutorial 7 - Enemies, Part 1: Static Observiers Adds Gargoyle Animator, Observer script and updates GameEnding. Moves the gargoyl so that it's chilling out ready to catch the player.

--- Assets/Scripts/GameEnding.cs
+++ Assets/Scripts/GameEnding.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameEnding : MonoBehaviour
 {
@@ -8,9 +9,16 @@ public class GameEnding : MonoBehaviour
     public float fadeDuration = 1f;
     public GameObject player;
     public CanvasGroup exitBackgroundImageCanvasGroup;
+    public CanvasGroup caughtBackgroundImageCanvasGroup;
 
     bool m_IsPlayerAtExit;
+    bool m_IsPlayerCaught;
     float m_Timer;
+
+    public void CaughtPlayer ()
+    {
+        m_IsPlayerCaught = true;
+    }
 
     private void OnTriggerEnter (Collider other)
     {
@@ -21,18 +29,29 @@ public class GameEnding : MonoBehaviour
     {
         if (m_IsPlayerAtExit) 
         {
-            EndLevel();
+            EndLevel(exitBackgroundImageCanvasGroup, false);
+        }
+        else if (m_IsPlayerCaught)
+        {
+            EndLevel(caughtBackgroundImageCanvasGroup, true);
         }
     }
 
-    void EndLevel ()
+    void EndLevel (CanvasGroup imageCanvasGroup, bool doRestart)
     {
         m_Timer += Time.deltaTime;
-        exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
+        imageCanvasGroup.alpha = m_Timer / fadeDuration;
 
         if (m_Timer > fadeDuration + displayImageDuration)
         {
-            Application.Quit();
+            if (doRestart)
+            {
+                SceneManager.LoadScene(0);
+            } 
+            else
+            {
+                Application.Quit();
+            }
         }
     }
 }
---
+++ Assets/Scripts/Observer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Observer : MonoBehaviour
+{
+    public GameEnding gameEnding;
+    public Transform player;
+
+    bool m_IsPlayerInRange;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        m_IsPlayerInRange |= other.transform == player;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        m_IsPlayerInRange &= other.transform != player;
+    }
+
+    private void Update()
+    {
+        if (m_IsPlayerInRange)
+        {
+            Vector3 direction = player.position - transform.position + Vector3.up;
+            Ray ray = new Ray(transform.position, direction);
+
+            if (Physics.Raycast(ray, out RaycastHit raycastHit))
+            {
+                if (raycastHit.collider.transform == player)
+                {
+                    gameEnding.CaughtPlayer();
+                }
+            }
+        }
+    }
+}
